a595a212f30b8ea2cc2774e736fb50062c4ba5ce
jayjgurung <newuser@jxxded-mbp.myfiosgateway.com>
2023-10-11 19:46:38 -0400

    +.py cake & spanish

---
+++ cake.py
@@ -0,0 +1,5 @@
+cakeorno = input("\nDo you want cake??\n")
+if (cakeorno == "yes"):
+    print("\nHere's you cake!\n")
+else:
+    print("\nNo cake for you!\n")
---
+++ spanish.py
@@ -0,0 +1,9 @@
+word = input("\nGive me a word in English:\n")
+if(word=="dog"):
+    print("\nperro!\n")
+elif(word=="cat"):
+    print("\ngato\n")
+elif(word=="horse"):
+    print("\ncaballo\n")
+else:
+    print("\nno entiendo!\n")
